6a64a8b07ec3e3ff2584c1aed9af2475fa152e18
KenLi8504 <kli22@stuy.edu>
2021-11-24 11:33:38 -0500

    Finally finished parse and counting arguments functions that would be able to ignore spaces

--- shell.c
+++ shell.c
@@ -9,9 +9,15 @@ char ** parse_args(char * line,int counter){
   char ** returnThis = malloc(8*(counter));
   int current = 0;
   while( (sub = strsep(&copyLine," ")) != NULL){
-    returnThis[current] = sub;
-    current++;
+    int value = *sub;
+    if (value != 0){
+      // printf("The value is %d\n",value);
+      // printf("The string is %s\n",sub);
+      returnThis[current] = sub;
+      current++;
+    }
   }
+  returnThis[current] = NULL;
   return returnThis;
 }
 
@@ -21,16 +27,23 @@ int countArgs(char * line){
   strcpy(copyLine,line);
   int counter = 0;
   while( (sub = strsep(&copyLine," ")) != NULL){
-    counter++;
+    int value = *sub;
+    if (value){
+      // printf("Good!\n");
+      counter++;
+    }
   }
-  return counter;
+  return counter+1;
 }
 
-int main(){
-  char *test = "ls -a -l";
+int main(int argc, char ** argv){
+  char *test = "echo hello";
   int length = countArgs(test);
+  printf("%d\n",length);
+  // printf("%d",strcmp(" ", strsep(&test," ")));
   char ** good = parse_args(test,length);
-  for (int i = 0; i < length; i++){
-    printf("The thing at good is %s\n",good[i]);
-  }
+  // for (int i = 0; i < length-1; i++){
+  //   printf("The thing at good is %s\n",good[i]);
+  // }
+  execvp(good[0],good);
 }
