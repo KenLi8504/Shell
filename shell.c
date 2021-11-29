d207a2e34c170c6d7e19909f81b31ce47b197aeb
KenLi8504 <kli22@stuy.edu>
2021-11-29 06:43:14 -0500

    Made progress on the seperating at commas, completed the fork aspect

--- shell.c
+++ shell.c
@@ -2,6 +2,35 @@
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/wait.h>
+
+
+int count_semicols(char *line){
+	int count = 0;
+	for (int i = 0; i < strlen(line); i++){
+		if (line[i] == ';'){
+			count++;
+		}
+	}
+	return count;
+}
+
+char ** split_at_semicolons(char *line,int num){
+  char *sub;
+  int current = 0;
+  char ** returnThis = malloc(8*num);
+  while( (sub = strsep(&line," ; ")) != NULL){
+    int value = *sub;
+    if (value != 0){
+      // printf("The value is %d\n",value);
+      // printf("The string is %s\n",sub);
+      returnThis[current] = sub;
+      current++;
+    }
+  }
+  return returnThis;
+}
+
 char ** parse_args(char * line,int counter){
   char *sub;
   char *copyLine = malloc (strlen(line));
@@ -36,14 +65,39 @@ int countArgs(char * line){
   return counter+1;
 }
 
+void execute(char ** good){
+  int fork1 = fork();
+  if (fork1){
+    wait(NULL);
+    return;
+  }
+  else{
+    execvp(good[0],good);
+    exit(0);
+  }
+}
+
 int main(int argc, char ** argv){
-  char *test = "echo hello";
-  int length = countArgs(test);
-  printf("%d\n",length);
-  // printf("%d",strcmp(" ", strsep(&test," ")));
-  char ** good = parse_args(test,length);
-  // for (int i = 0; i < length-1; i++){
-  //   printf("The thing at good is %s\n",good[i]);
-  // }
-  execvp(good[0],good);
+  while (1){
+    char *blank = malloc(10000);
+    fgets(blank,10000,stdin);
+
+    char *test = malloc(10000);
+    //fgets(test,10000,stdin);
+    test = "ls -al; echo hello";
+    int semicolons = count_semicols(test);
+    printf("There are %d semicolons in this line\n",semicolons);
+    char ** testing = malloc(1000);
+    testing = split_at_semicolons(test,semicolons);
+    printf("Value:  %p\n", testing);
+    // /printf("The strings are %d\n",strcmp(test,test2));
+    for (int i = 0; i < semicolons; i++){
+      int length = countArgs(testing[i]);
+      char ** good = parse_args(testing[i],length);
+      execute(good);
+    }
+    // for (int i = 0; i < length-1; i++){
+    //   printf("The thing at good is %s\n",good[i]);
+    // }
+  }
 }
